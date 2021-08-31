04f8693e614259a4e48a6106d47a71420c460ccd
HeXianGang <793678945@qq.com>
2021-08-31 21:57:58 +0800

    init project

---
+++ src/main/java/com/blob/newhandblob/NewhandblobApplication.java
@@ -0,0 +1,13 @@
+package com.blob.newhandblob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewhandblobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewhandblobApplication.class, args);
+    }
+
+}
